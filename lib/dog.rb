bf5183a45684c269ff6821c2f0be00be06a08336
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-08 16:58:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,6 +31,8 @@ class Dog
     result = DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     result
+    
+    binding.pry
   end
   
   

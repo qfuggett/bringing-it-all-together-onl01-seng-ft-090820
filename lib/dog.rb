049bcaae79a6d91d5fdb2be539b18b3d83e2582d
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-08 17:10:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
   def save
     sql = "INSERT INTO dogs(name, breed) VALUES (?, ?)"
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][id]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
   
   
